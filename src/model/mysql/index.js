0cd64beb9e64a05914da0460376a2753964d4d1b
yunaichun <yunaichun@qingting.fm>
2020-11-17 10:40:48 +0800

    feat: sequelize 添加端口连接

--- src/model/mysql/index.js
+++ src/model/mysql/index.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 const config = require('../../config');
 
-const { host, user, password, database } = config.mysql;
+const { host, port, user, password, database } = config.mysql;
 
 const sequelize = new Sequelize(database, user, password, {
   host,
+  port,
   dialect: 'mysql',
 });
 
