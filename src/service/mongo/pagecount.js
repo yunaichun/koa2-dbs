1917159eed8bbfae0b049f5dbbb1539c651fa65e
yunaichun <yunaichun@qingting.fm>
2022-08-05 13:53:14 +0800

    feat(NoTicket): add page view count

--- src/service/mongo/pagecount.js
+++ src/service/mongo/pagecount.js
@@ -3,13 +3,13 @@ const Model = require('../../model/mongo/pagecount');
 module.exports = {
   async updateAndFind(params) {
     const result = await Model.findOne(params);
-    console.log(123, params, result)
     if (result) {
       const { count } = result;
       await Model.update(params, { count: count + 1 }, { multi: true });
     } else {
-      if (!params.url) throw Error('url is required');
-      if (url.indexOf('answera.top') === -1) throw Error('only count answera.top site');
+      const { url } = params;
+      if (!url) throw Error('url is required');
+      if (url.indexOf('answera.top') === -1) throw Error('your website is not allowed to count');
       const newModal = new Model({ ...params, count: 1 });
       await newModal.save();
     }
